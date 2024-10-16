2ed43530e165ceb5edfe8f5fd22522760c310107
WIN-J2TB9CU2OL5\ICT02_15 <yoonda000@gmail.com>
2024-10-16 11:37:20 +0900

    게시판

--- services/BoardService.jsx
+++
@@ -1,9 +0,0 @@
-import http from "./HttpCommon";
-
-const getPagingList = (path = "/boards/list", search = "") => {
-  return http.get(path + search);
-};
-
-export default {
-  getPagingList,
-};
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,20 +1,49 @@
-import Footer from "../components/layout/Footer";
 import { createBrowserRouter } from "react-router-dom";
 import App from "./../App";
-import LottoBall from "../components/lotto/LottoBall";
 import LottoPage from "../components/pages/LottoPage";
 import RspPage from "../components/pages/RspPage";
+import BoardListPage from "./../components/pages/BoardListPage";
+import BoardWritePage from "../components/pages/BoardWritePage";
 
 const routes = [
   {
     path: "/",
     element: <App />,
-    loader: () => "로또",
+    loader: () => "가위바위보",
     children: [
       {
         path: "/",
-        loader: () => "로또",
+        loader: () => "가위바위보",
         element: <RspPage />,
+      },
+    ],
+  },
+  {
+    path: "/lotto",
+    element: <App />,
+    loader: () => "로또",
+    children: [
+      {
+        path: "/lotto",
+        loader: () => "로또",
+        element: <LottoPage />,
+      },
+    ],
+  },
+  {
+    path: "/boards",
+    element: <App />,
+    loader: () => "게시판",
+    children: [
+      {
+        path: "/boards",
+        loader: () => "게시판",
+        element: <BoardListPage />,
+      },
+      {
+        path: "/boards/write",
+        loader: () => "글쓰기",
+        element: <BoardWritePage />,
       },
     ],
   },
